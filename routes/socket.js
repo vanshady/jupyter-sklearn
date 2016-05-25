f7f66251b76959e4752f2b3dbfac3499f50468ab
Minwei Xu <faceswilliam@gmail.com>
2016-05-25 00:20:10 -0700

    autosklearn

--- routes/socket.js
+++ routes/socket.js
@@ -19,6 +19,24 @@ import {
 // The base url of the notebook server.
 const BASE_URL = 'http://localhost:8888';
 
+const sklearn_code = ['import autosklearn.classification',
+  'import sklearn.datasets',
+  'digits = sklearn.datasets.load_digits()',
+  'X = digits.data',
+  'y = digits.target',
+  'import numpy as np',
+  'indices = np.arange(X.shape[0])',
+  'np.random.shuffle(indices)',
+  'X = X[indices]',
+  'y = y[indices]',
+  'X_train = X[:1000]',
+  'y_train = y[:1000]',
+  'X_test = X[1000:]',
+  'y_test = y[1000:]',
+  'automl = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=60, per_run_time_limit=40)',
+  'automl.fit(X_train, y_train)',
+  'print(automl.score(X_test,y_test))'].join('\n');
+  
 // Get a list of available sessions and connect to one.
 listRunningSessions({
   baseUrl: BASE_URL
